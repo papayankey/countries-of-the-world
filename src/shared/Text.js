0d043d7c648a07e77a949ac8d365b2f66de2f444
iambenbright <yankeybenneth@gmail.com>
2020-07-22 20:10:27 +0000

    feat: adds text component

---
+++ src/shared/Text.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+import { typography, compose } from 'styled-system';
+import css from '@styled-system/css';
+import propTypes from '@styled-system/prop-types';
+
+import { textColor } from '../customFns';
+
+export const Text = styled('p')(
+  css({
+    fontWeight: 'extraLight',
+    fontSize: 3,
+    color: 'gray300',
+  }),
+  compose(typography, textColor)
+);
+
+Text.propTypes = {
+  ...propTypes.typography,
+};
---
+++ src/shared/index.js
@@ -0,0 +1,5 @@
+import { Box } from './Box';
+import { Flex } from './Flex';
+import { Text } from './Text';
+
+export { Box, Flex, Text };

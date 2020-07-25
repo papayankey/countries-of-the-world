4c713693e9f734e3b07fd88d875ade5264bcdb78
iambenbright <yankeybenneth@gmail.com>
2020-07-25 05:36:43 +0000

    refactor to use original data

--- src/components/CardList/Card.js
+++ src/components/CardList/Card.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import PropType from 'prop-types';
 
 import { Box, Text } from '../../shared';
+import { useNumberFormat } from '../../hooks/useNumberFormat';
 
-export function Card() {
+export function Card({ data }) {
+  let { name, population, region, capital } = data;
+  population = useNumberFormat(population);
+
   return (
     <Box
       bg="white"
@@ -15,22 +20,26 @@ export function Card() {
       <Box position="relative" p="30%" bg="gray300" borderRadius="none"></Box>
       <Box px="xl" py="4xl">
         <Text as="h2" fontWeight="Bolder" fontSize="6">
-          Germany
+          {name}
         </Text>
         <Box mt="2xl">
           <Text fontWeight="extraBold">
-            Population: <Text as="span">81, 770, 900</Text>
+            Population: <Text as="span">{population}</Text>
           </Text>
           <Box mt="md"></Box>
           <Text fontWeight="extraBold">
-            Region: <Text as="span">Europe</Text>
+            Region: <Text as="span">{region}</Text>
           </Text>
           <Box my="md"></Box>
           <Text fontWeight="extraBold">
-            Capital: <Text as="span">Berlin</Text>
+            Capital: <Text as="span">{capital}</Text>
           </Text>
         </Box>
       </Box>
     </Box>
   );
 }
+
+Card.propType = {
+  data: PropType.object.isRequired,
+};
--- src/components/CardList/index.js
+++ src/components/CardList/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { Card } from './Card';
-import { Grid } from '../../shared';
+import { Grid, Box } from '../../shared';
+
+import { data } from '../../data';
 
 function CardList() {
+  const [countryData, setCountryData] = useState([]);
+
+  useEffect(() => {
+    function getCountryData(data) {
+      setCountryData(data);
+    }
+    getCountryData(data);
+  });
+
   return (
     <Grid
       mt="2xl"
       gridTemplateColumns="repeat(auto-fill, minmax(200px, 1fr))"
       gridGap="40px"
     >
-      {[...new Array(7)].map((_, idx) => (
-        <Card key={idx} />
-      ))}
+      {countryData.length > 0 ? (
+        countryData.map(data => {
+          return (
+            <Link to="/country">
+              <Card key={data.name} data={data} />
+            </Link>
+          );
+        })
+      ) : (
+        <Box as="p">No Data!</Box>
+      )}
     </Grid>
   );
 }

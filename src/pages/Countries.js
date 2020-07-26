5389fe6cc8ca37bed05cad9db78f74613fac892f
iambenbright <yankeybenneth@gmail.com>
2020-07-26 17:56:15 +0000

    refactor

--- src/pages/Countries.js
+++ src/pages/Countries.js
@@ -1,13 +1,12 @@
 import React from 'react';
 
 import { Search, Select, CardList } from '../components';
-import { Flex } from '../shared';
-import { Container } from '../shared';
+import { Flex, Container } from '../shared';
 
 function Countries() {
   return (
     <Container>
-      <Flex mt="2xl" justifyContent="Space-between" flexWrap="wrap">
+      <Flex mt="3xl" justifyContent="Space-between" flexWrap="wrap">
         <Search />
         <Select />
       </Flex>

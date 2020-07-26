02c898c3bfe0dc07aac295d55b42d0d0f6923f29
iambenbright <yankeybenneth@gmail.com>
2020-07-26 17:54:01 +0000

    Update base styles

--- src/shared/Grid.js
+++ src/shared/Grid.js
@@ -1,15 +1,20 @@
+import React from 'react';
 import styled from 'styled-components';
 import { grid } from 'styled-system';
 import propTypes from '@styled-system/prop-types';
 
 import { Box } from './Box';
 
-export const Grid = styled(Box)(
+const StyledGrid = styled(Box)(
   {
     display: 'grid',
   },
   grid
 );
+
+export function Grid({ children, ...other }) {
+  return <StyledGrid {...other}>{children}</StyledGrid>;
+}
 
 Grid.propTypes = {
   ...propTypes.grid,

310934e98070665487a90e25bdaf58fbf790a035
iambenbright <yankeybenneth@gmail.com>
2020-07-31 00:25:43 +0000

    feat: adds filter by region

--- src/pages/Countries.js
+++ src/pages/Countries.js
@@ -1,54 +1,83 @@
-import React, { useState } from 'react';
-import { useQuery } from 'react-query';
-import { Link } from 'react-router-dom';
-
-import { Search, Select, Card, Pagination } from '../components';
+import React, { useState, useEffect, useCallback } from 'react';
+// import { useQuery } from 'react-query';
+import { Search, Select, CardList } from '../components';
 import { Flex, Container, Grid, Box } from '../shared';
 
-function Countries() {
-  const [region, setRegion] = useState('All');
-  let { data, isLoading, isError } = useQuery('countries', fetchCountries, {
-    cacheTime: 1000 * 60 * 10,
-  });
+// data cache
+const cache = {};
 
-  async function fetchCountries() {
-    const response = await (
-      await fetch('https://restcountries.eu/rest/v2/all')
-    ).json();
-    return response;
+function Countries() {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [currentRegion, setCurrentRegion] = useState(
+    () => cache['region'] || 'All'
+  );
+
+  useEffect(() => {
+    async function fetchCountries() {
+      console.log('fetching new data....');
+      const response = await fetch('https://restcountries.eu/rest/v2/all');
+      const result = await response.json();
+      setData(result);
+      setIsLoading(false);
+      // cache data
+      cache['All'] = result;
+    }
+
+    if (cache['All']) {
+      setData(cache['All']);
+      setIsLoading(false);
+    } else {
+      fetchCountries();
+    }
+  }, []);
+
+  const filterData = useCallback(
+    function filterData() {
+      return data.filter(item => item.region === currentRegion);
+    },
+    [data, currentRegion]
+  );
+
+  let filteredData = null;
+  if (data) {
+    if (currentRegion === 'All') filteredData = data;
+    else {
+      filteredData = filterData();
+    }
   }
 
-  const handleFilterByRegion = filter => setRegion(filter);
+  function changeRegion(region) {
+    if (currentRegion !== region) {
+      setCurrentRegion(region);
+      cache['region'] = region;
+    }
+  }
 
   return (
     <Container>
       <Flex mt="3xl" justifyContent="Space-between" flexWrap="wrap">
         <Search />
-        <Select handleFilterByRegion={handleFilterByRegion} />
+        <Select onChangeRegion={changeRegion} />
       </Flex>
-      <Grid
-        mt="4xl"
-        gridTemplateColumns="repeat(auto-fill, minmax(250px, 1fr))"
-        gridGap="40px"
-      >
-        {isLoading && <Box as="p">Loading...</Box>}
-        {isError && <Box as="p">Oops! something went wrong</Box>}
-        {data &&
-          data.map(country => {
-            return (
-              <Link key={country.name} to={`/countries/${country.name}`}>
-                <Card country={country} />
-              </Link>
-            );
-          })}
-      </Grid>
-      {data && (
-        <Pagination
-          totalCountries={data.length}
-          // pageNumber={currentPage}
-          // countriesPerPage={countriesPerPage}
-          // paginate={paginate}
-        />
+      {isLoading && (
+        <Box as="p" mt="4xl">
+          Loading...
+        </Box>
+      )}
+      {/* {isError && (
+        <Box as="p" mt="4xl">
+          Oops! something wrong
+        </Box>
+      )} */}
+      {filteredData && (
+        <Grid
+          mt="4xl"
+          gridTemplateColumns="repeat(auto-fill, minmax(250px, 1fr))"
+          gridGap="40px"
+        >
+          <CardList countries={filteredData} />
+        </Grid>
       )}
     </Container>
   );
